08f8c3dee412a12a3da1c4f7a9595993600e95c4
Lawlan1 <154861110+Lawlan1@users.noreply.github.com>
2024-01-06 23:09:19 +0000

    Update admin.py

--- chat/admin.py
+++ chat/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import ChatMessage, Resource
+from .models import ChatMessage, Resource, UserFeedback
 
 
 admin.site.register(ChatMessage)
+admin.site.register(UserFeedback)
 admin.site.register(Resource)
 admin.site.site_header = 'Chatbot Administration'
 
