72f01fb22f7bc08435009ef7f96acf9ba17e9484
Lawlan1 <154861110+Lawlan1@users.noreply.github.com>
2024-01-06 23:09:45 +0000

    Update forms.py

--- chat/forms.py
+++ chat/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from .models import UserFeedback
 
 class ChatbotForm(forms.Form):
     message = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Type your message here...'}))
+
+class UserFeedbackForm(forms.ModelForm):
+    class Meta:
+        model = UserFeedback
+        fields = ['feedback_text']
